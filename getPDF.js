0281452cb34aab8fc23863970788478edb36519d
Daryl Jewkes <github@daryljewkes.com>
2015-06-01 16:27:32 +0100

    initial

--- getPDF.js
+++ getPDF.js
@@ -2,12 +2,19 @@ var phantom = require('phantom');
 
 phantom.create(function (ph) {
   ph.createPage(function (page) {
-    page.open("http://www.google.com", function (status) {
-      console.log("opened google? ", status);
-      page.evaluate(function () { return document.title; }, function (result) {
-        console.log('Page title is ' + result);
-        ph.exit();
-      });
-    });
+    // page.open("http://www.google.com", function (status) {
+    //   console.log("opened google? ", status);
+    //   page.evaluate(function () { return document.title; }, function (result) {
+    //     console.log('Page title is ' + result);
+    //     ph.exit();
+    //   });
+    // });
+
+  	page.viewportSize = { width: 1920, height: 1080 };
+	page.open("http://www.google.com", function start(status) {
+	  page.render('google_home.pdf', {format: 'pdf', quality: '100'});
+	  ph.exit();
+	});
+
   });
 });
