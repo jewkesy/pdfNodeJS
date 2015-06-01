e260a0e4e7e3edc51bc168883d7672f9e6b90d45
Daryl Jewkes <github@daryljewkes.com>
2015-06-01 17:17:37 +0100

    initial

--- getPDF.js
+++ getPDF.js
@@ -10,11 +10,16 @@ phantom.create(function (ph) {
     //   });
     // });
 
-  	page.viewportSize = { width: 1920, height: 1080 };
-	page.open("http://www.google.com", function start(status) {
-	  page.render('google_home.pdf', {format: 'pdf', quality: '100'});
-	  ph.exit();
+  	// page.viewportSize = { width: 1920, height: 1080 };
+  	page.set('paperSize', { format: 'A4', orientation: 'portrait', margin: '1cm'});
+	// var theUrl = "http://jewkesy.github.io/FormulaOne/#/2015/drivers";
+	var theUrl = "http://www.daryljewkes.com"
+	// var theUrl = "http://getbootstrap.com/css"
+	page.open(theUrl, function start(status) {
+		setTimeout(function () {
+			page.render('render.pdf', {format: 'pdf', quality: '100'});
+	  		ph.exit();
+		}, 500);
 	});
-
   });
 });
