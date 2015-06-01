550b3169665dd1d793797fdcef8b3d454db942df
Daryl Jewkes <github@daryljewkes.com>
2015-06-01 15:54:27 +0100

    initial

--- getPDF.js
+++ getPDF.js
@@ -1,29 +1,13 @@
-var path = require('path')
-var childProcess = require('child_process')
-var phantomjs = require('phantomjs')
-var binPath = phantomjs.path
- 
+var phantom = require('phantom');
 
-
-
-var webPage = require('webpage');
-var page = webPage.create();
-
-page.viewportSize = { width: 1920, height: 1080 };
-page.open("http://www.google.com", function start(status) {
-  page.render('google_home.jpeg', {format: 'jpeg', quality: '100'});
-  phantom.exit();
+phantom.create(function (ph) {
+  ph.createPage(function (page) {
+    page.open("http://www.google.com", function (status) {
+      console.log("opened google? ", status);
+      page.evaluate(function () { return document.title; }, function (result) {
+        console.log('Page title is ' + result);
+        ph.exit();
+      });
+    });
+  });
 });
-
-
- 
-// var childArgs = [
-//   path.join(__dirname, 'phantomjs-script.js'),
-//   'some other argument (passed to phantomjs script)'
-// ]
- 
-// childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-//   // handle results 
-// })
-
-
